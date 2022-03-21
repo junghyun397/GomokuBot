44d3f7a8f8ee9ca1c72156008b3237948533ef71
junghyun397 <me@do1ph.in>
2022-03-21 11:20:31 +0900

    refactor: simplicity either type

--- src/main/kotlin/utility/lang.kt
+++ src/main/kotlin/utility/lang.kt
@@ -5,23 +5,18 @@ import org.slf4j.LoggerFactory
 
 sealed class Either <out L, out R> {
 
-    abstract val isLeft: Boolean
-    abstract val isRight: Boolean
+    val isLeft: Boolean get() = this is Left
+
+    val isRight: Boolean get() = this is Right
 
     inline fun <T> fold(onLeft: (L) -> T, onRight: (R) -> T): T = when (this) {
-        is Left -> onLeft(value)
-        is Right -> onRight(value)
+        is Left -> onLeft(this.value)
+        is Right -> onRight(this.value)
     }
 
-    data class Left<out L>(val value: L) : Either<L, Nothing>() {
-        override val isLeft = true
-        override val isRight = false
-    }
+    data class Left<out L>(val value: L) : Either<L, Nothing>()
 
-    data class Right<out R>(val value: R) : Either<Nothing, R>() {
-        override val isLeft = false
-        override val isRight = true
-    }
+    data class Right<out R>(val value: R) : Either<Nothing, R>()
 
 }
 
