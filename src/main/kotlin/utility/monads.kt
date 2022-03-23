c07d95c09c9fc95f7f8de556e240b03af199c78d
junghyun397 <me@do1ph.in>
2022-03-24 07:17:01 +0900

    feat: add map and flatMap function in option type

--- src/main/kotlin/utility/monads.kt
+++ src/main/kotlin/utility/monads.kt
@@ -8,9 +8,21 @@ sealed class Option<out T> {
 
     fun getOrNull(): T? = (this as Some).value
 
+    inline fun <R> map(mapper: (T) -> R): Option<R> =
+        when(this) {
+            is Some -> Some(mapper(this.value))
+            is Empty -> this
+        }
+
+    inline fun <R> flatMap(mapper: (T) -> Option<R>): Option<R> =
+        when(this) {
+            is Some -> mapper(this.value)
+            is Empty -> this
+        }
+
     inline fun <R> fold(onDefined: (T) -> R, onEmpty: () -> R): R =
         when(this) {
-            is Some -> onDefined(value)
+            is Some -> onDefined(this.value)
             is Empty -> onEmpty()
         }
 
