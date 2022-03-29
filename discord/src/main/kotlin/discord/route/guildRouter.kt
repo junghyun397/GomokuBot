2b0db3ee70cfbe8b5228c935582607d9a81e64be
junghyun397 <me@do1ph.in>
2022-03-29 16:47:29 +0900

    feat: add language matching

--- discord/src/main/kotlin/discord/route/guildRouter.kt
+++ discord/src/main/kotlin/discord/route/guildRouter.kt
@@ -1,6 +1,9 @@
 package discord.route
 
+import core.database.DatabaseManager
+import core.interact.i18n.Language
 import core.interact.reports.GuildJoinReport
+import core.session.entities.GuildConfig
 import discord.interact.GuildManager
 import discord.interact.InteractionContext
 import discord.interact.message.DiscordMessageBinder
@@ -8,11 +11,28 @@ import discord.interact.message.DiscordMessagePublisher
 import discord.interact.message.MessageActionRestActionAdaptor
 import kotlinx.coroutines.reactor.mono
 import net.dv8tion.jda.api.Permission
+import net.dv8tion.jda.api.Region
+import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
 import reactor.util.function.Tuple2
-import utils.monads.IO
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+private suspend fun retrieveRegion(guild: Guild): Region =
+    suspendCoroutine { control ->
+        guild.retrieveRegions()
+            .map { it.first() }
+            .queue { control.resume(it) }
+    }
+
+private fun matchLanguage(region: Region): Language =
+    when (region) {
+        Region.SOUTH_KOREA, Region.VIP_SOUTH_KOREA -> Language.KOR
+        Region.JAPAN, Region.VIP_JAPAN -> Language.JPN
+        else -> Language.ENG
+    }
 
 fun guildJoinRouter(context: InteractionContext<GuildJoinEvent>): Mono<Tuple2<InteractionContext<GuildJoinEvent>, Result<GuildJoinReport>>> =
     Mono.zip(context.toMono(), mono { runCatching {
@@ -22,18 +42,25 @@ fun guildJoinRouter(context: InteractionContext<GuildJoinEvent>): Mono<Tuple2<In
             true
         }
 
+        val defaultRegion = retrieveRegion(context.event.guild)
+
+        val matchedLanguage = matchLanguage(defaultRegion)
+
+        DatabaseManager.updateGuildConfig(
+            context.botContext.databaseConnection, context.guild,
+            GuildConfig(language = matchedLanguage)
+        )
+
         val helpSent = context.event.guild.defaultChannel?.let { channel ->
             val messagePublisher: DiscordMessagePublisher = { msg -> MessageActionRestActionAdaptor(channel.sendMessage(msg)) }
 
             GuildManager.permissionSafeRun(channel, Permission.MESSAGE_SEND) {
-                IO { }
-                    .flatMap { DiscordMessageBinder.bindAboutBot(messagePublisher, context.guildConfig.language.container).map { it.retrieve() } }
+                DiscordMessageBinder.bindAboutBot(messagePublisher, context.guildConfig.language.container).map { it.retrieve() }
                     .flatMap { DiscordMessageBinder.bindCommandGuide(messagePublisher, context.guildConfig.language.container).map { it.retrieve() } }
                     .flatMap { DiscordMessageBinder.bindStyleGuide(messagePublisher, context.guildConfig.language.container).map { it.retrieve() } }
                     .flatMap { DiscordMessageBinder.bindLanguageGuide(messagePublisher).map { it.launch() } }
-                    .run()
             }
         }?.isDefined
 
-        GuildJoinReport(commandInserted, helpSent)
+        GuildJoinReport(commandInserted, helpSent, defaultRegion.name, matchedLanguage)
     } } )
