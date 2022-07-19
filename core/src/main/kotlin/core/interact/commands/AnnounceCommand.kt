512976c84cb2881d7d7cda2490a160684c3868a0
junghyun397 <junghyun397@gmail.com>
2022-07-19 21:33:34 +0900

    fix: fix announce command logic

--- core/src/main/kotlin/core/interact/commands/AnnounceCommand.kt
+++ core/src/main/kotlin/core/interact/commands/AnnounceCommand.kt
@@ -5,15 +5,12 @@ import core.assets.Guild
 import core.assets.User
 import core.database.repositories.AnnounceRepository
 import core.database.repositories.UserProfileRepository
-import core.interact.Order
 import core.interact.i18n.Language
 import core.interact.message.MessageAdaptor
 import core.interact.message.MessageProducer
 import core.interact.message.MessagePublisher
-import core.interact.reports.CommandReport
 import core.session.entities.GuildConfig
 import kotlinx.coroutines.Deferred
-import utils.structs.IO
 import utils.structs.flatMap
 import utils.structs.map
 
@@ -28,27 +25,27 @@ class AnnounceCommand(override val name: String, private val command: Command) :
         message: Deferred<MessageAdaptor<A, B>>,
         publisher: MessagePublisher<A, B>,
         editPublisher: MessagePublisher<A, B>
-    ) : Result<Pair<IO<List<Order>>, CommandReport>> {
-        val thenUser = user.copy(announceId = AnnounceRepository.getLatestAnnounceId(bot.dbConnection))
+    ) = this.command
+        .execute(bot, config, guild, user, producer, message, publisher, editPublisher)
+        .map { (originalIO, originalReport) ->
+            val thenUser = user.copy(announceId = AnnounceRepository.getLatestAnnounceId(bot.dbConnection))
 
-        UserProfileRepository.upsertUser(bot.dbConnection, thenUser)
+            UserProfileRepository.upsertUser(bot.dbConnection, thenUser)
 
-        return this.command
-            .execute(bot, config, guild, user, producer, message, publisher, editPublisher)
-            .map { (originalIO, report) ->
-                val io = AnnounceRepository.getAnnouncesSince(bot.dbConnection, user.announceId ?: -1)
-                    .map { announces ->
-                        producer.produceAnnounce(
-                            publisher,
-                            config.language.container,
-                            announces[config.language] ?: announces[Language.ENG]!!
-                        ).map { it.launch() }
-                    }
-                    .reduce { acc, io -> acc.flatMap { io } }
-                    .flatMap { originalIO }
+            val io = AnnounceRepository.getAnnouncesSince(bot.dbConnection, user.announceId ?: -1)
+                .map { announces ->
+                    producer.produceAnnounce(
+                        publisher,
+                        config.language.container,
+                        announces[config.language] ?: announces[Language.ENG]!!
+                    ).map { it.launch() }
+                }
+                .reduce { acc, io -> acc.flatMap { io } }
+                .flatMap { originalIO }
 
-                io to report
-            }
-    }
+            val report = originalReport.copy(commandName = "$name${originalReport.commandName}")
+
+            io to report
+        }
 
 }
