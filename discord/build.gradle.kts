e62cdb5190ea8f7fbb7ad83e3349d1388b442064
junghyun397 <junghyun397@gmail.com>
2024-01-18 05:12:03 +0900

    chore: update dependencies

--- discord/build.gradle.kts
+++ discord/build.gradle.kts
@@ -13,11 +13,11 @@ dependencies {
     implementation(project(":utils"))
     implementation(project(":core"))
 
-    implementation("net.dv8tion:JDA:5.0.0-beta.17")
-    implementation("com.github.minndevelopment:jda-ktx:9370cb1")
+    implementation("net.dv8tion:JDA:5.0.0-beta.19")
+    implementation("com.github.minndevelopment:jda-ktx:e8b0a7080cc234727abf5a83871ae0c9b6603a58")
     implementation("com.github.minndevelopment:jda-reactor:e01a635")
 
-    implementation("ch.qos.logback:logback-classic:1.4.0")
+    implementation("ch.qos.logback:logback-classic:1.4.12")
 }
 
 application {
